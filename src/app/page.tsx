a0b7c42a3d5a6394160990fcfd74dbcf1182b965
Amanpreet <akaur.manpreet@rwit.io>
2025-06-28 19:46:43 +0530

    Initial setup: Next.js + Tailwind + ShadCN UI

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+
+
+    <main className="text-center mt-20">
+      <h1 className="text-5xl font-bold text-green-600">✅ Working!</h1>
+    </main>
+
+  );
+}
